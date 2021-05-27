c6bb32ec4829768f19a0b3199ee5b5fa689f13c0
Richard Eckart de Castilho <richard.eckart@gmail.com>
2021-05-27 22:29:56 +0200

    #23 - Add ability to configure custom branding CSS
    - Check for "branding.css" file in the application home and if it exists, load it and use it when rendering pages

--- inception-humanprotocol-app/src/main/java/io/github/reckart/inception/humanprotocol/app/Starter.java
+++ inception-humanprotocol-app/src/main/java/io/github/reckart/inception/humanprotocol/app/Starter.java
@@ -16,27 +16,75 @@
  */
 package io.github.reckart.inception.humanprotocol.app;
 
+import static de.tudarmstadt.ukp.clarin.webanno.support.SettingsUtil.getApplicationHome;
+
+import java.io.File;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.Page;
+import org.apache.wicket.behavior.Behavior;
+import org.apache.wicket.markup.head.CssHeaderItem;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.resource.FileSystemResourceReference;
 import org.springframework.boot.autoconfigure.AutoConfigurationExcludeFilter;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.TypeExcludeFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.FilterType;
+
+import com.giffing.wicket.spring.boot.context.extensions.WicketApplicationInitConfiguration;
 
 import de.tudarmstadt.ukp.inception.INCEpTION;
 
 @SpringBootApplication
 @AutoConfigurationPackage(basePackages = { "io.github.reckart.inception.humanprotocol" })
 @EntityScan(basePackages = { "io.github.reckart.inception.humanprotocol" })
-@ComponentScan(basePackages = { "io.github.reckart.inception.humanprotocol" },
-    excludeFilters = {
+@ComponentScan(basePackages = { "io.github.reckart.inception.humanprotocol" }, excludeFilters = {
         @Filter(type = FilterType.REGEX, pattern = ".*AutoConfiguration"),
         @Filter(type = FilterType.CUSTOM, classes = TypeExcludeFilter.class),
         @Filter(type = FilterType.CUSTOM, classes = AutoConfigurationExcludeFilter.class) })
-public class Starter extends INCEpTION
+public class Starter
+    extends INCEpTION
 {
+    @Bean
+    public WicketApplicationInitConfiguration branding()
+    {
+        return new WicketApplicationInitConfiguration()
+        {
+            @Override
+            public void init(WebApplication aWebApplication)
+            {
+                File brandingCss = new File(getApplicationHome(), "branding.css");
+
+                if (!brandingCss.exists()) {
+                    return;
+                }
+
+                aWebApplication.getComponentInstantiationListeners().add(component -> {
+                    if (component instanceof Page) {
+                        component.add(new Behavior()
+                        {
+                            private static final long serialVersionUID = 5519463574787275765L;
+
+                            @Override
+                            public void renderHead(Component aComponent, IHeaderResponse aResponse)
+                            {
+                                aResponse.render(
+                                        CssHeaderItem.forReference(new FileSystemResourceReference(
+                                                "branding.css", brandingCss.toPath())));
+                            }
+                        });
+                    }
+                });
+            }
+        };
+    }
+
     public static void main(String[] args) throws Exception
     {
         run(args, Starter.class);
